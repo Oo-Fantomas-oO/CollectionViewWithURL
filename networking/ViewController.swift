c5cf0d73fd0c85d7b571ef5b1aeb362be747eddb
Fantomas <1234556789>
2020-10-23 11:26:43 +0300

    initial commit

---
+++ networking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  networking
+//
+//  Created by Onopriienko.Sergii on 23.10.2020.
+//  Copyright © 2020 Onopriienko.Sergii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
