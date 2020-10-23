c601b76d01cab277ede50f51338c165336215eab
Fantomas <1234556789>
2020-10-23 12:05:02 +0300

    add interface in IB, add outlets and action to ViewController

--- networking/ViewController.swift
+++ networking/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var lable: UILabel!
+    @IBOutlet weak var getImageButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getImagePressed(_ sender: Any) {
+    }
+    
 }
 
